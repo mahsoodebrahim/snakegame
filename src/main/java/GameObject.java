75794b3204e23590eafd7c925738e642fa847ef3
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-10-17 21:15:43 -0700

    Remove isAlive attritbute of GameObject class

--- src/main/java/GameObject.java
+++ src/main/java/GameObject.java
@@ -5,16 +5,12 @@ import com.codegym.engine.cell.Game;
 public abstract class GameObject {
     protected int x;
     protected int y;
-    protected boolean isAlive;
     protected String symbol;
 
     public GameObject(int x, int y, String symbol) {
         this.x = x;
         this.y = y;
         this.symbol = symbol;
-
-        // Set by default, GameObject is alive (true)
-        isAlive = true;
     }
 
     abstract public void draw(Game game);

31326c68c05021084bcede539bf95c7d8e7a0dce
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-10-17 21:13:27 -0700

    Check if snake it out of bounds

--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -24,17 +24,35 @@ public class Snake {
     }
 
     public void draw(Game game) {
-        for (SnakePart snakePart: snakeParts) {
+        for (SnakePart snakePart : snakeParts) {
             snakePart.draw(game);
         }
     }
 
     public void move() {
+        // Get current snake head
         SnakePart curSnakeHead = snakeParts.get(0);
-        curSnakeHead.symbol = snakeBodySymbol;
+
+        // Create a new snake head in the snake's travel direction
         SnakePart newSnakeHead = new SnakePart(curSnakeHead.x - 1, curSnakeHead.y, snakeHeadSymbol, snakeColor);
+
+        // Check if newly created head causes snake to be out of bounds
+        if (isSnakeOutOfBounds(newSnakeHead)) {
+            return;
+        }
+
+        // Update current head symbol to a snake body symbol
+        curSnakeHead.symbol = snakeBodySymbol;
+
+        // Add new snake head to beginning on snakeParts list
         snakeParts.add(0, newSnakeHead);
 
+        // Remove last element of snakeParts list
         snakeParts.remove(snakeParts.size() - 1);
+    }
+
+    public boolean isSnakeOutOfBounds(SnakePart snakeHead) {
+        // Snake if out of bounds conditions
+        return snakeHead.x < 0 || snakeHead.x >= SnakeGame.WIDTH || snakeHead.y < 0 || snakeHead.y >= SnakeGame.HEIGHT;
     }
 }
